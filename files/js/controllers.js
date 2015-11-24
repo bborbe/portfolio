28040b138893b6cc5687f0bf8b5d5580177bfa10
Benjamin Borbe <bborbe@rocketnews.de>
2015-11-24 19:12:45 +0100

    Bind forward and backwards keys

--- files/js/controllers.js
+++ files/js/controllers.js
@@ -5,32 +5,69 @@ angular.module('portfolioControllers', []);
 angular.module('portfolioControllers').controller('StartCtrl', ['$scope', '$q', '$log', '$interval', 'ConfigService', function ($scope, $q, $log, $interval, ConfigService) {
   $log.debug("started");
 
+  $scope.$on('keypress', function (onEvent, keypressEvent) {
+    if (keypressEvent.keyCode == 39 || keypressEvent.charCode == 32) {
+      $scope.next();
+      $scope.noop();
+    }
+    if (keypressEvent.keyCode == 37) {
+      $scope.last();
+      $scope.noop();
+    }
+  });
+
   $scope.init = function () {
     $log.debug('init');
     ConfigService.get().then(function successCallback(config) {
       $scope.config = config;
       angular.element(window.document)[0].title = config.title + ' - ' + config.subtitle + ' ' + config.subtext;
-      $interval($scope.next, 5000);
-      $scope.next();
+      $interval($scope.interfalFunc, 5000);
+      $scope.show(0);
     }, function errorCallback(response) {
-      $log.error('log config failed');
+      $log.error('log config failed: ' + response);
     });
   };
 
   var current = 0;
 
+
+  $scope.interfalFunc = function () {
+    $scope.next();
+  };
+
+  $scope.noop = function () {
+    $scope.interfalFunc = function () {
+      $scope.interfalFunc = function () {
+        $scope.next();
+      };
+    };
+  };
+
   $scope.next = function () {
-    var url = $scope.config.images[current];
+    current = (current + 1) % $scope.config.images.length;
+    $scope.show(current);
+  };
+
+  $scope.last = function () {
+    if (current == 0) {
+      current = $scope.config.images.length - 1;
+    } else {
+      current = (current - 1);
+    }
+    $scope.show(current);
+  };
+
+  $scope.show = function (pos) {
+    var url = $scope.config.images[pos];
     $scope.preload(url).then(function () {
       $scope.image = 'url("' + url + '")';
       $log.debug("image = " + $scope.image);
-      current = (current + 1) % $scope.config.images.length;
     });
   };
 
   $scope.preload = function (url) {
-    var deferred = $q.defer(),
-      image = new Image();
+    var deferred = $q.defer();
+    var image = new Image();
     image.src = url;
     if (image.complete) {
       deferred.resolve();
@@ -43,7 +80,7 @@ angular.module('portfolioControllers').controller('StartCtrl', ['$scope', '$q', 
       });
     }
     return deferred.promise;
-  }
+  };
 
   $scope.init();
 }]);
--- files/js/directives.js
+++ files/js/directives.js
@@ -1,3 +1,16 @@
 'use strict';
 
 angular.module('portfolioDirectives', []);
+
+
+angular.module('portfolioDirectives', []).directive('keypressEvents', ['$document', '$rootScope', function ($document, $rootScope) {
+  return {
+    restrict: 'A',
+    link: function () {
+      $document.bind('keypress', function (e) {
+        $rootScope.$broadcast('keypress', e);
+      });
+    }
+  };
+}
+]);
