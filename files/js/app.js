5754fd1c0905cde2f3be202fa66a421271f5eb71
Benjamin Borbe <bborbe@rocketnews.de>
2015-12-03 11:08:20 +0100

    send pageview after create

--- files/js/app.js
+++ files/js/app.js
@@ -22,16 +22,16 @@ angular.module('portfolioApp').config(['$routeProvider', function ($routeProvide
 angular.module('portfolioApp').run(['$rootScope', '$location', '$window', '$log', 'ConfigService', function ($rootScope, $location, $window, $log, ConfigService) {
   ConfigService.get().then(function successCallback(config) {
     var id = config['google-analytics-tracking-id'];
-    //$log.debug('tracking id = ' + id);
     if ($window.ga) {
+      $log.debug('tracking id = ' + id);
       $window.ga('create', id);
     }
-  });
-  $rootScope.$on('$locationChangeSuccess', function (event) {
-    //$log.debug('$locationChangeSuccess: ' + $location.path());
-    if ($window.ga) {
-      //$log.debug('send pageview to google: ' + $location.path());
-      $window.ga('send', 'pageview', {page: $location.path()});
-    }
+    $rootScope.$on('$locationChangeSuccess', function (event) {
+      //$log.debug('$locationChangeSuccess: ' + $location.path());
+      if ($window.ga) {
+        $log.debug('send pageview to google: ' + $location.path());
+        $window.ga('send', 'pageview', {page: $location.path()});
+      }
+    });
   });
 }]);
