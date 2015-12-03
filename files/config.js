2f77f3aad6135b7736d333bf7cc882bcb92ff898
Benjamin Borbe <bborbe@rocketnews.de>
2015-12-03 11:29:07 +0100

    use config value

--- files/config.js
+++ files/config.js
@@ -2,27 +2,26 @@
 
 angular.module('portfolioConfig', []);
 
-angular.module('portfolioConfig').value('config',
-  {
-    "google-analytics-tracking-id": "abc",
-    "title": "Benjamin Borbe",
-    "subtitle": "Fotografie",
-    "subtext": "die Spass macht",
-    "images": [
-      "images/start/image1.jpg",
-      "images/start/image2.jpg",
-      "images/start/image3.jpg"
-    ],
-    "navi": [
-      {
-        "href": "https://500px.com/ben",
-        "name": "Portfolio"
-      },
-      {
-        "href": "mailto:info@benjamin-borbe.de",
-        "name": "Kontakt"
-      }
-    ]
-  });
+angular.module('portfolioConfig').value('config', {
+  "google-analytics-tracking-id": "abc",
+  "title": "Benjamin Borbe",
+  "subtitle": "Fotografie",
+  "subtext": "die Spass macht",
+  "images": [
+    "images/start/image1.jpg",
+    "images/start/image2.jpg",
+    "images/start/image3.jpg"
+  ],
+  "navi": [
+    {
+      "href": "https://500px.com/ben",
+      "name": "Portfolio"
+    },
+    {
+      "href": "mailto:info@benjamin-borbe.de",
+      "name": "Kontakt"
+    }
+  ]
+});
 
 
