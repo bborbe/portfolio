abfcdeb976e625b5e3ec570d4d7027c0a737143b
Benjamin Borbe <bborbe@rocketnews.de>
2016-07-25 23:00:03 +0200

    move handler and handler_finder

--- handler/portfolio_handler.go
+++ handler/portfolio_handler.go
@@ -3,13 +3,13 @@ package handler
 import (
 	"net/http"
 
-	"github.com/bborbe/handler_finder/part"
+	"github.com/bborbe/http_handler/cachingheader"
+	"github.com/bborbe/http_handler/contenttype"
+	"github.com/bborbe/http_handler/fallback"
+	log_handler "github.com/bborbe/http_handler/log"
+	"github.com/bborbe/http_handler/static"
+	"github.com/bborbe/http_handler_finder/part"
 	"github.com/bborbe/log"
-	"github.com/bborbe/server/handler/cachingheader"
-	"github.com/bborbe/server/handler/contenttype"
-	"github.com/bborbe/server/handler/fallback"
-	log_handler "github.com/bborbe/server/handler/log"
-	"github.com/bborbe/server/handler/static"
 )
 
 var logger = log.DefaultLogger
